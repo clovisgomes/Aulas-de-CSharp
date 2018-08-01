1f6ee21d4bf7d87793129fffca0f3523572b439a
1717877 <1717877@DASOE6715A>
2018-07-31 21:59:42 -0300

    op1

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -4,20 +4,31 @@ namespace Aula3107_Console
 {
     class Program
     {
-        public static int Id { get; set; }
+        public static int n1 { get; set; }
+        public static int n2 { get; set; }
 
         static void Main(string[] args)
-        {             
+        {
+            Console.WriteLine("Hello World!");
 
-        Console.WriteLine("Hello World!");
+            Console.WriteLine("\n");
+            op = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Digite um numero");
-            Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite 1° número:");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Você digitou: " +n1);
 
-            Console.WriteLine("Você digitou: " + Id);
+            Console.WriteLine("Digite 2° número:");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Você digitou: " +n2);
 
             Console.ReadKey();
+        }
 
+        static int imprimirSoma (int n1, int n2)
+        {
+            int res = 
         }
     }
 }
+
