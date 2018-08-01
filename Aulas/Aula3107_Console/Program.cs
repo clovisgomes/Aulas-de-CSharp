6065319025217b3abb9de80fca84fc7c80a39a68
1717877 <1717877@DASOE6715A>
2018-07-31 21:26:02 -0300

    interação com usuário converventendo string para int

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula3107_Console
 {
     class Program
     {
+        public static int Id { get; set; }
+
         static void Main(string[] args)
-        {
+        {             
+
+        Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Digite um numero");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou: " + Id);
+
+            Console.ReadKey();
+
         }
     }
 }
