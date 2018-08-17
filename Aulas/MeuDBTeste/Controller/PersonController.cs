7837b09a1402522cf2e443c8da49a724a9c8c6ca
Clóvis Fernando Gomes <cfogomes@hotmail.com>
2018-08-16 21:55:13 -0300

    1

---
+++ Aulas/MeuDBTeste/Controller/PersonController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuDBTeste.Controller
+{
+    class PersonController
+    {
+        void Inserir(Person p)
+        {
+            MeuDBContainer contexto = new MeuDBContainer();
+            contexto.Person.Add(p);
+            contexto.SaveChanges();
+        }
+        List<Person> ListarTodosPerson()
+        {
+            MeuDBContainer contexto = new MeuDBContainer();
+            return contexto.Person.ToList();
+        }
+        Person BuscarPorID(int id)
+        {
+            MeuDBContainer contexto = new MeuDBContainer();
+            return contexto.Person.Find(id);
+        }
+
+        void Excluir(int id)
+        {
+            Person pExcluir = BuscarPorID(id);
+
+            if(pExcluir != null)
+            {
+                MeuDBContainer contexto = new MeuDBContainer();
+                contexto.Person.Remove(pExcluir);
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Aulas/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿RESTORE DATABASE ClovisApplicationTesting
+FROM DISK = 'C:\Users\user\Downloads\AdventureWorks2016.bak'
+
+WITH MOVE 'AdventureWorks2016_Data' TO 'C:\Users\user\source\repos\Aulas-de-CSharp\Aulas\MeuDBTeste\NewFolder1\AdventureWorks2016.mdf',
+MOVE 'AdventureWorks2016_Log' TO 'C:\Users\user\source\repos\Aulas-de-CSharp\Aulas\MeuDBTeste\NewFolder1\AdventureWorks2016.ldf',
+REPLACE
