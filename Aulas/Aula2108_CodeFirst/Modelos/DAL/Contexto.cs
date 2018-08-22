00be29df28c9534ef6156d054f39c3b22fd8b75f
Clóvis Fernando Gomes <cfogomes@hotmail.com>
2018-08-21 22:20:32 -0300

    DBSet

--- Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -1,9 +1,19 @@
 ﻿
 
+using System.Data.Entity;
+
 namespace Aula2108_CodeFirst.Modelos.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        //Construtor
+        public Contexto() : base("nomeStringConexao")
+        {
+                
+        }
 
+         DbSet<Categoria> Categorias { get; set; }
+
+         DbSet<Produto> Produtos { get; set; }
     }
 }
